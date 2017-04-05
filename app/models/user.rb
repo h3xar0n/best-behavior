1af5339ee0b92d8d598841c4f555927e9b5fc248
h3xar0n <aron@glambyadelina.com>
2017-04-05 18:19:39 -0400

    Finalized way to check for email validation regardless of case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMIAL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i 
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMIAL_REGEX }
+                    format: { with: VALID_EMIAL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,4 +53,18 @@ class UserTest < ActiveSupport::TestCase
         "Address #{invalid_addresses.inspect} should be invalid."
     end
   end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end  
 end
