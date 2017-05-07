3df3902d02827cbf45ed27f52fae8bc4494ec156
h3xar0n <aron@glambyadelina.com>
2017-05-07 10:42:23 -0400

    Added admins, default is false, and attackers cannot edit admin attribute via the web

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create!(name:  "Maximus",
              email: "maximus@optimus.net",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
