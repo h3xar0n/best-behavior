7986603ffa0505b350b19505de76b23ee122740c
h3xar0n <aron@glambyadelina.com>
2017-05-25 16:23:00 -0400

    User/relationship associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,9 @@ class User < ApplicationRecord
 
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 
   # Returns the hash digest of the given string.
   def User.digest(string)
